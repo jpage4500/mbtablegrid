e0103da12eddedec23f55554085b21c2d94337e1
Brandon Evans <brandon@brandonevans.ca>
2014-11-03 17:53:03 -0700

    Extract attributed string creation into attributedStringValue method
    This was just done as cleanup while working on performance fixes

--- MBTableGridHeaderCell.m
+++ MBTableGridHeaderCell.m
@@ -91,38 +91,38 @@
 	[self drawInteriorWithFrame:cellFrame inView:controlView];
 }
 
-- (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
-{
-    
+- (NSAttributedString *)attributedStringValue {
 	NSFont *font = [NSFont labelFontOfSize:[NSFont labelFontSize]];
 	NSColor *color = [NSColor controlTextColor];
-	NSDictionary *attributes = @{NSFontAttributeName: font, NSForegroundColorAttributeName: color};
-	NSAttributedString *string = [[NSAttributedString alloc] initWithString:[self stringValue] attributes:attributes];
-	
+	NSDictionary *attributes = @{ NSFontAttributeName: font, NSForegroundColorAttributeName: color };
+
+	return [[NSAttributedString alloc] initWithString:[self stringValue] attributes:attributes];
+}
+
+- (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
+{
 	static CGFloat TEXT_PADDING = 6;
-
 	NSRect textFrame;
-
+	CGSize stringSize = self.attributedStringValue.size;
 	if (self.orientation == MBTableHeaderHorizontalOrientation) {
-		textFrame = NSMakeRect(cellFrame.origin.x + TEXT_PADDING, cellFrame.origin.y + (cellFrame.size.height - [string size].height)/2, cellFrame.size.width - TEXT_PADDING, [string size].height);
+		textFrame = NSMakeRect(cellFrame.origin.x + TEXT_PADDING, cellFrame.origin.y + (cellFrame.size.height - stringSize.height)/2, cellFrame.size.width - TEXT_PADDING, stringSize.height);
 	} else {
-		textFrame = NSMakeRect(cellFrame.origin.x + (cellFrame.size.width-[string size].width)/2, cellFrame.origin.y + (cellFrame.size.height - [string size].height)/2, [string size].width, [string size].height);
+		textFrame = NSMakeRect(cellFrame.origin.x + (cellFrame.size.width - stringSize.width)/2, cellFrame.origin.y + (cellFrame.size.height - stringSize.height)/2, stringSize.width, stringSize.height);
 	}
-	
+
 	[[NSGraphicsContext currentContext] saveGraphicsState];
+
 	NSShadow *textShadow = [[NSShadow alloc] init];
 	[textShadow setShadowOffset:NSMakeSize(0,-1)];
 	[textShadow setShadowBlurRadius:0.0];
 	[textShadow setShadowColor:[NSColor colorWithDeviceWhite:1.0 alpha:0.8]];
 	[textShadow set];
-	
-//	[string drawInRect:textFrame];
-	[string drawWithRect:textFrame options:NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin];
+
+	[self.attributedStringValue drawWithRect:textFrame options:NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin];
 	
 	[[NSGraphicsContext currentContext] restoreGraphicsState];
 	
 	NSRect sortIndicatorRect = [self sortIndicatorRectForBounds:cellFrame];
-	
 	[self.sortIndicatorImage drawInRect:sortIndicatorRect];
 	
 }
